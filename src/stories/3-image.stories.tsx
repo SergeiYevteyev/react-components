e21e587110c60e937612c35bcf136c51c04f34fc
Sergejs Jevtejevs <sergei.yevteyev@gmail.com>
2020-02-19 01:14:05 +0200

    storybook added

---
+++ src/stories/3-image.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Image } from '../components/images/image';
+
+export default {
+    title: 'Image',
+    component: Image,
+  };
+
+export const randomImage = () => <Image description="some random man" filelocation="https://source.unsplash.com/random/400x200" />;
