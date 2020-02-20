15c17471bd7631ea2678ed48f0237cebe9de5891
Sergejs Jevtejevs <sergei.yevteyev@gmail.com>
2020-02-21 00:15:15 +0200

    not needed components were removed

--- src/stories/3-image.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Image from '../components/images/image';
-
-export default {
-    title: 'Image',
-    component: Image,
-  };
-
-export const image = () => <Image description="some random man" filelocation="https://source.unsplash.com/random/400x200" />;
