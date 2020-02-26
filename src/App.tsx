00fa7daa48b78c43ac75b077c2746f460d77864f
Sergejs Jevtejevs <sergei.yevteyev@gmail.com>
2020-02-27 01:30:58 +0200

    finally real layout started to appear

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>      
-    </div>
+    <Container>
+    <Row>
+      <Col xs={8}>
+
+      </Col>
+        <Col xs={4}>
+            <Image alt="some random image" src="https://source.unsplash.com/random/200x200" roundedCircle />
+        </Col>
+    </Row>
+</Container>
   );
 }
 
