70c01e7db18da3a723f30c45cbff73fd04c8c22b
Sergejs Jevtejevs <sergei.yevteyev@gmail.com>
2020-02-27 22:04:54 +0200

    horizontal separator added

---
+++ src/components/linebreak.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+function LineBreak() {
+    return (
+        <Row>
+            <Col>
+               <hr />
+            </Col>
+        </Row>
+    )
+}
+
+export default LineBreak
---
+++ src/stories/linebreak.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LineBreak from '../components/linebreak'
+
+export default {
+    title: 'LineBreak',
+    component: LineBreak,
+  };
+
+export const lineBreakComponent = () => <LineBreak />
