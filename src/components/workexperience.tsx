9bed4aa22f301df358d48de01767f425513b62a4
Sergejs Jevtejevs <ext-sergejs.jevtejevs@intrum.com>
2020-03-03 10:40:04 +0200

    workexperience implemented and added to story book

---
+++ src/components/workexperience.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+export interface workExperienceProps {
+    items: workExperience[]
+}
+
+interface workExperience {
+    position: string,
+    where: string,
+    techStack: string,
+    shortStory: string
+}
+
+export function createWorkExperience(
+    position: string,
+    where: string,
+    techStack: string,
+    shortStory: string) : workExperience {
+        return {
+            position: position,
+            shortStory: shortStory,
+            techStack: techStack,
+            where: where
+        }
+    }
+
+export function createBuilder() : workExperienceBuilder {
+    return new workExperienceBuilder();
+}
+
+class workExperienceBuilder {
+    private experience: workExperience[]
+
+    /**
+     *
+     */
+    constructor() {
+        this.experience = []
+    }
+
+    Add(workExperience: workExperience) {
+        this.experience.push(workExperience)
+    }
+
+    Build() : workExperienceProps {
+        return {
+            items: this.experience
+        }
+    }
+}
+
+function WorkExperience(props: workExperienceProps) {
+    const section = renderWorkExperienceSection(props.items)
+
+return (<div>{section}</div>)
+}
+
+function renderWorkExperienceSection(items: workExperience[]) {
+    const components: JSX.Element[] = []
+
+    for (var i = 0; i < items.length; ++i) {
+        components.push(renderWorkExperience(items[i]));
+    }
+
+    return components
+}
+
+function renderWorkExperience(workExperience: workExperience) {
+    return (
+        <div>
+            <h3>{workExperience.position}</h3>
+            <h4>{workExperience.where}</h4>
+            <h5>{workExperience.techStack}</h5>
+            <span>{workExperience.shortStory}</span>
+        </div>
+    )
+}
+
+
+export default WorkExperience;
