60052adf41cb53b65ba63706c2d79ae5350cbe0d
Sergejs Jevtejevs <ext-sergejs.jevtejevs@intrum.com>
2020-03-05 17:35:27 +0200

    story added

---
+++ src/stories/education.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Education, * as we from '../components/education'
+
+export default {
+    title: 'education',
+    component: Education,
+  };
+
+function retrieveEducation() : we.educationProps{
+    var builder = we.createBuilder()
+    builder.Add(we.createeducation(
+        'ed1',
+        'Rigas Tehniska Universitate',
+        'Bachelor\'s degree, automation and computer engineering - computer control systems',
+        '2008-2010'))
+
+    builder.Add(we.createeducation(
+            'ed2',
+            'Rigas Tehniska Universitate',
+            'Master\'s degree, automation and computer engineering - computer control systems',
+            '2008-2010'))
+
+    return builder.Build()
+}
+
+export const educationComponent = () => <Education items={retrieveEducation().items} />
